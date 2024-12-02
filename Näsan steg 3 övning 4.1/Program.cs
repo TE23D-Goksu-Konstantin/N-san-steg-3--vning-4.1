0565c7198ddb2fcdcb2525290a646d1fbf295d13
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-12-02 09:33:28 +0100

    klar

---
+++ Näsan steg 3 övning 4.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+
+    static int Triangle(int a, int b)
+    {
+        return(a - b);
+    }
+
+    static void Main(string[] args)
+    {
+                bool potato = true;
+
+        while(potato == true){
+        Console.WriteLine("Skriv en siffra som sedan ska subtraheras med en annan siffra som du skriver in.");
+        string c = Console.ReadLine();
+
+
+        if(int.TryParse(c, out int d))
+        {
+        Console.WriteLine(d);
+        }else{
+            Console.WriteLine("Fel!");
+            continue;
+        }
+
+        bool tomato = true;
+        while(tomato == true){
+        Console.WriteLine("Skriv in den andra nu");
+        string e = Console.ReadLine();
+
+        if(int.TryParse(e, out int f))
+        {
+            Console.WriteLine(f);
+        }else{
+            Console.WriteLine("Fel!");
+            continue;
+        }
+        
+
+        Console.WriteLine(Triangle(d, f));
+        Console.ReadLine();
+        }
+        }
+    }
+}
